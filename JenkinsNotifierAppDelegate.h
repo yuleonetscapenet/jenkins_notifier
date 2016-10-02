2e61cbd67d4a6be0339650bccbb6fdfa8ee6a172
leo yu <leo@stuff.com>
2016-10-02 12:56:47 -0700

    Another test commit.

--- JenkinsNotifierAppDelegate.h
+++ JenkinsNotifierAppDelegate.h
@@ -4,6 +4,6 @@
 
 @property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
 @property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
-@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext; // Test
 
 @end
